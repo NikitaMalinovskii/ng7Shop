1c5a18c044dac00dfac7d4faf1ae4334987bf3df
F4ilFish <47733499+F4ilFish@users.noreply.github.com>
2022-03-25 10:40:26 +0700

    test1

--- assets/js/navbarScroll.js
+++
@@ -1,6 +0,0 @@
-// Javascript for navigation bar effect on scroll
-
-window.addEventListener('scroll', function(){
-    var navbar = document.querySelector(".navbar");
-    navbar.classList.toggle('sticky', window.scrollY);
-})
